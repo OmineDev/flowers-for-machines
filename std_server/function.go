9d73c58e85b0ade4f53aa051e0f86ac3c9b99a23
Happy2018new <chenbo79800@163.com>
2025-07-09 14:28:04 +0800

    Standard server: Fix place large chest

--- std_server/function.go
+++ std_server/function.go
@@ -136,7 +136,9 @@ func PlaceNBTBlock(c *gin.Context) {
 func PlaceLargeChest(c *gin.Context) {
 	mu.Lock()
 	defer mu.Unlock()
+
 	var request PlaceLargeChestRequest
+	var offset protocol.BlockPos
 
 	err := c.BindJSON(&request)
 	if err != nil {
@@ -148,16 +150,33 @@ func PlaceLargeChest(c *gin.Context) {
 	}
 
 	// Step 1: Prepare
-	chestBlockStates := utils.ParseBlockStatesString(request.BlockStatesString)
 	center := console.Center()
+	chestBlockStates := utils.ParseBlockStatesString(request.BlockStatesString)
+	direction, _ := chestBlockStates["minecraft:cardinal_direction"].(string)
+
+	// Step 2: Get offset
+	switch direction {
+	case "north", "south":
+		offset = protocol.BlockPos{1, 0, 0}
+	case "east", "west":
+		offset = protocol.BlockPos{0, 0, 1}
+	case "":
+		c.JSON(http.StatusOK, PlaceLargeChestResponse{
+			Success:   false,
+			ErrorInfo: fmt.Sprintf("Invalid chest block states %#v", request.BlockStatesString),
+		})
+		return
+	}
+
+	// Step 3: Calculate chest position
 	pairleadPos := protocol.BlockPos{
 		center[0], center[1] + 1, center[2],
 	}
 	pairedPos := protocol.BlockPos{
-		center[0] + 1, center[1] + 1, center[2],
+		center[0] + offset[0], center[1] + 1, center[2] + offset[2],
 	}
 
-	// Step 2.1: Place pairlead chest
+	// Step 4.1: Place pairlead chest
 	if !request.PairleadChestStructureExist {
 		err = gameInterface.SetBlock().SetBlock(pairleadPos, request.BlockName, request.BlockStatesString)
 		if err != nil {
@@ -192,7 +211,7 @@ func PlaceLargeChest(c *gin.Context) {
 		States:      chestBlockStates,
 	}
 
-	// Step 2.2: Place paired chest
+	// Step 4.2: Place paired chest
 	if !request.PairedChestStructureExist {
 		err = gameInterface.SetBlock().SetBlock(pairedPos, request.BlockName, request.BlockStatesString)
 		if err != nil {
@@ -221,8 +240,8 @@ func PlaceLargeChest(c *gin.Context) {
 		}
 	}
 
-	// Step 3: Backup loaded chests
-	tempStructure, err := gameInterface.StructureBackup().BackupOffset(pairleadPos, [3]int32{1, 0, 0})
+	// Step 5: Backup loaded chests
+	tempStructure, err := gameInterface.StructureBackup().BackupOffset(pairleadPos, offset)
 	if err != nil {
 		c.JSON(http.StatusOK, PlaceLargeChestResponse{
 			Success:   false,
@@ -232,7 +251,7 @@ func PlaceLargeChest(c *gin.Context) {
 	}
 	defer gameInterface.StructureBackup().DeleteStructure(tempStructure)
 
-	// Step 4.1: Clean loaded chests
+	// Step 6.1: Clean loaded chests
 	err = gameInterface.Commands().SendSettingsCommand(
 		fmt.Sprintf(
 			"fill %d %d %d %d %d %d air",
@@ -249,7 +268,7 @@ func PlaceLargeChest(c *gin.Context) {
 		return
 	}
 
-	// Step 4.2: Wait clean down
+	// Step 6.2: Wait clean down
 	err = gameInterface.Commands().AwaitChangesGeneral()
 	if err != nil {
 		c.JSON(http.StatusOK, PlaceLargeChestResponse{
@@ -261,7 +280,7 @@ func PlaceLargeChest(c *gin.Context) {
 	nearBlock = console.NearBlockByIndex(nbt_console.ConsoleIndexCenterBlock, protocol.BlockPos{0, 1, 0})
 	*nearBlock = block_helper.Air{}
 
-	// Step 5.1: Revert loaded chests on console center
+	// Step 7.1: Revert loaded chests on console center
 	err = gameInterface.StructureBackup().RevertStructure(tempStructure, console.Center())
 	if err != nil {
 		c.JSON(http.StatusOK, PlaceLargeChestResponse{
@@ -271,8 +290,8 @@ func PlaceLargeChest(c *gin.Context) {
 		return
 	}
 
-	// Step 5.2: Sync changes to console
-	nearBlock = console.NearBlockByIndex(nbt_console.ConsoleIndexCenterBlock, protocol.BlockPos{1, 0, 0})
+	// Step 7.2: Sync changes to console
+	nearBlock = console.NearBlockByIndex(nbt_console.ConsoleIndexCenterBlock, offset)
 	console.UseHelperBlock(nbt_console.RequesterUser, nbt_console.ConsoleIndexCenterBlock, block_helper.ComplexBlock{
 		KnownStates: true,
 		Name:        request.BlockName,
@@ -284,13 +303,13 @@ func PlaceLargeChest(c *gin.Context) {
 		States:      chestBlockStates,
 	}
 
-	// Step 6.1: Clone revert structures to ~ ~1 ~
+	// Step 8.1: Clone revert structures to ~ ~1 ~
 	err = gameInterface.Commands().SendSettingsCommand(
 		fmt.Sprintf(
 			"clone %d %d %d %d %d %d %d %d %d",
 			center[0], center[1], center[2],
-			center[0]+1, center[1], center[2],
-			center[0], center[1]+1, center[2],
+			center[0]+offset[0], center[1], center[2]+offset[2],
+			pairleadPos[0], pairleadPos[1], pairleadPos[2],
 		),
 		true,
 	)
@@ -302,7 +321,7 @@ func PlaceLargeChest(c *gin.Context) {
 		return
 	}
 
-	// Step 6.2: Wait clone down
+	// Step 8.2: Wait clone down
 	err = gameInterface.Commands().AwaitChangesGeneral()
 	if err != nil {
 		c.JSON(http.StatusOK, PlaceLargeChestResponse{
@@ -318,8 +337,8 @@ func PlaceLargeChest(c *gin.Context) {
 		States:      chestBlockStates,
 	}
 
-	// Step 7: Get final structure (that included a large chest)
-	finalStructure, err := gameInterface.StructureBackup().BackupOffset(pairleadPos, [3]int32{1, 0, 0})
+	// Step 9: Get final structure (that included a large chest)
+	finalStructure, err := gameInterface.StructureBackup().BackupOffset(pairleadPos, offset)
 	if err != nil {
 		c.JSON(http.StatusOK, PlaceLargeChestResponse{
 			Success:   false,
@@ -328,7 +347,7 @@ func PlaceLargeChest(c *gin.Context) {
 		return
 	}
 
-	// Step 8.1: Clean loaded large chest
+	// Step 10.1: Clean loaded large chest
 	err = gameInterface.Commands().SendSettingsCommand(
 		fmt.Sprintf(
 			"fill %d %d %d %d %d %d air",
@@ -345,7 +364,7 @@ func PlaceLargeChest(c *gin.Context) {
 		return
 	}
 
-	// Step 8.2: Wait clean down
+	// Step 10.2: Wait clean down
 	err = gameInterface.Commands().AwaitChangesGeneral()
 	if err != nil {
 		c.JSON(http.StatusOK, PlaceLargeChestResponse{
